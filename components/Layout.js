fb09f14e226e3c4795a0e4430dd95b6a8ef1104f
imadbg01 <imadbg101@gmail.com>
2021-12-15 02:07:47 +0100

    create components

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,13 @@
-import Nav from "./Nav"
-import styles from "../styles/Layout.module.css"
-
+import styles from "../styles/Layout.module.css";
+import Image from "next/image";
+import bg from "../img/pattern-background-desktop.svg";
 const Layout = ({ children }) => {
-    return (
-        <>
-            <Nav />
-            <dev className={styles.container}>
-                <main className={styles.main}>
-                    <h1>hello there , how's Live </h1>
-                    {children}
-                </main>
-            </dev>
-        </>
-    )
-}
+  return (
+    <main className={styles.mainBg}>
+      <Image src={bg} className={styles.img} />
+      {children}
+    </main>
+  );
+};
 
-export default Layout
+export default Layout;
